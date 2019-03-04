bbec60a3a8bc24cd494c945203bbcd64761a5146
Glen <glen_sale@yahoo.com>
2019-03-04 10:56:35 -0800

    it will create a config file for firebase API and firestore to store in database and collections

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBgmEasGVGUPPqZpKgG0pV-9n6QSomUbgc",
+    authDomain: "capstone-1996.firebaseapp.com",
+    databaseURL: "https://capstone-1996.firebaseio.com",
+    projectId: "capstone-1996",
+    storageBucket: "capstone-1996.appspot.com",
+    messagingSenderId: "488403964252"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({timeStampsInSnapShots: true})
+
+export default firebase;
