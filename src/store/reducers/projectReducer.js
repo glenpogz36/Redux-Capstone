bd123817cd5ed2fb1f0281e1c5659522c10d0f87
glen <glen_sale@yahoo.com>
2019-03-03 19:45:10 -0800

    it wil create initialState and pass down data by mapping through unique key id of projects

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,6 +1,8 @@
 const initState = {
     projects: [
-        { id: '1', title: 'Test1', content: 'Content1' }
+        { id: '1', title: 'Test1', content: 'Content1' },
+           { id: '2', title: 'Test1', content: 'Content2' },
+              { id: '3', title: 'Test1', content: 'Content3' }
     ]
 }
 const projectReducer = (state = initState, action) => {
