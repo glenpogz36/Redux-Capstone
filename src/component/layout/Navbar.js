b39833a3a29485b11e5b5b379d25e8e0761b93cf
Glen <glen_sale@yahoo.com>
2019-03-04 14:56:18 -0800

    it will delete some file and re added again to solve some bug issue

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Login from './Login'
-import Logout from './Logout'
+import LogIn from './LogIn'
+import LogOut from './LogOut'
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper #263238 blue-grey darken-4">
             <div className="container">
                 <Link to='/' className="brand-logo">Capstone</Link>
-                <Login />
-                <Logout />
+                <LogIn />
+                <LogOut />
             </div>
         </nav>
     )
