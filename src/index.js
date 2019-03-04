8b42ff2069997596921a5851026fd307e68d8fec
Glen <glen_sale@yahoo.com>
2019-03-04 11:42:04 -0800

    it will install firebase and firestore packages and import data from App to firebase

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { reduxFireStore, getFireStore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase, reactReduxFirebase } from 'react-redux-firebase'
+import firebaseConfig from '../src/config/firebaseConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument({ getFireStore, getFirebase })),
+        reduxFireStore(firebaseConfig),
+        reactReduxFirebase(firebaseConfig),
+    )
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
