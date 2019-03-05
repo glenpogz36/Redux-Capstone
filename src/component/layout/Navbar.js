745dbdbfa6bf112349fc73de3347d385aef93992
Glen <glen_sale@yahoo.com>
2019-03-05 10:56:42 -0800

    it will connect firebase and ation (auth) to dispatch credentials of user Login Datas

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import LogIn from './LogIn'
 import LogOut from './LogOut'
+import {connect } from 'react-redux'
 
 const Navbar = () => {
     return (
@@ -14,5 +15,10 @@ const Navbar = () => {
         </nav>
     )
 }
+const mapStateToProps = (state) =>{
+    console.log(state);
+    return{
 
-export default Navbar
+    }
+}
+export default connect(mapStateToProps) (Navbar)
