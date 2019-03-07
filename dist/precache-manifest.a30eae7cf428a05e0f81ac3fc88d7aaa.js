b7645c43df4c6274c7427f8f2a143519074f2554
Glen Sale <glen_sale@yahoo.com>
2019-03-07 12:53:00 -0800

    it will fix orderlist by asencding order and will try to deploy to firebase if app is working

---
+++ dist/precache-manifest.a30eae7cf428a05e0f81ac3fc88d7aaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ebe4f6005102888935f6",
+    "url": "/static/js/main.ebe4f600.chunk.js"
+  },
+  {
+    "revision": "d06f91e375b169f997f9",
+    "url": "/static/js/2.d06f91e3.chunk.js"
+  },
+  {
+    "revision": "ebe4f6005102888935f6",
+    "url": "/static/css/main.fa9ff028.chunk.css"
+  },
+  {
+    "revision": "b04c6a8a55fd5cebbdb56f7d7a75f21f",
+    "url": "/index.html"
+  }
+];
