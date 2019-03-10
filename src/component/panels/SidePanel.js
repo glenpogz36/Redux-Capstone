38840da6771fe1980d48d5aa9311bfa24485acfa
glen <glen_sale@yahoo.com>
2019-03-09 20:38:04 -0800

    it will create ChatBox for messages

---
+++ src/component/panels/SidePanel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+
+
+
+class SidePanel extends Component {
+    render() {
+        
+
+        return (
+          <div></div>
+        )
+    }
+}
+
+export default SidePanel;
